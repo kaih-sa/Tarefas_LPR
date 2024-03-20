37d9ac0453eba44640e9902fda2bd9b902769836
kaih-sa <kaiohesasa@gmail.com>
2024-03-20 19:53:45 -0300

    Create Exercicio 2.cs

---
+++ Aula05/Exercicio 2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class ex_2
+{
+    static void Main()
+    {
+        int a, b;
+
+        Console.Write("Insira o valor de A: ");
+         a = int.Parse(Console.ReadLine());
+        Console.Write("Insira o valor de B: ");
+        b = int.Parse(Console.ReadLine());
+
+        if (a%b == 0 || b%a == 0)
+        {
+            Console.WriteLine("Eles são múltiplos");
+        }
+        else
+        {
+            Console.WriteLine("Eles não são múltiplos");
+        }
+
+    }
+}
